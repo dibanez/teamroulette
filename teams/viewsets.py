44a8af0872243144360d9fdbf7d3829d55ff96c2
David Ibanez Cerdeira <dibanez@gmail.com>
2014-10-04 14:11:26 +0200

    add filter queryset

--- teams/viewsets.py
+++ teams/viewsets.py
@@ -6,6 +6,11 @@ class TeamViewSet(ModelViewSet):
     model = models.Team
     permission_classes = (permissions.IsOwnerPermission, )
 
+    #Modificamos la query para devolver los registros del usuario.
+    def filter_queryset(self, queryset):
+        queryset = super(TeamViewSet, self).filter_queryset(queryset)
+        return queryset.filter(owner=self.request.user)
+
 
 class PlayerViewSet(ModelViewSet):
     model = models.Player
