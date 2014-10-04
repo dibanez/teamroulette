5188b4382bee6a5382bbdb83d73778a539b01b62
David Ibanez Cerdeira <dibanez@gmail.com>
2014-10-04 16:29:34 +0200

    reparir settings for pep8

--- teamroulette/settings.py
+++ teamroulette/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os, dj_database_url
+import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
