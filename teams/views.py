e14dc3d0e1e2d7eff9826239159219a481871609
David Ibanez Cerdeira <dibanez@gmail.com>
2014-10-04 10:10:45 +0200

    add context

--- teams/views.py
+++ teams/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from . import forms
+
 
 def main(request):
+    form = forms.TeamForm()
+    dictionary = {
+        'name': 'David',
+        'form': form,
+    }
     return render(request,
                   'teams/index.html',
-                  dictionary={'name': 'David'})
+                  dictionary=dictionary)
